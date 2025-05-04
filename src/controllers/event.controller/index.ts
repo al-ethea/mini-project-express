f171f6e21b3645df69b5a954241887dee03e4370
al-ethea <alethea.azka3@gmail.com>
2025-05-04 19:29:07 +0700

    fix: merge

--- src/controllers/event.controller/index.ts
+++ src/controllers/event.controller/index.ts
@@ -10,60 +10,6 @@ export const getCreatedEvents = async (
   next: NextFunction
 ) => {
   try {
-    const {
-      name,
-      bannerUrl,
-      city,
-      venue,
-      date,
-      category,
-      price,
-      description,
-      availableSeats,
-      type,
-      artistId,
-      organizerProfileId,
-    } = req.body;
-
-    // Konversi dan validasi data
-    const formattedDate = new Date(date); // cukup langsung new Date
-    const parsedPrice = Number(price);
-    const parsedAvailableSeats = Number(availableSeats);
-    const parsedArtistId = Number(artistId);
-    const parsedOrganizerProfileId = Number(organizerProfileId);
-
-    if (isNaN(parsedPrice) || isNaN(parsedAvailableSeats) || isNaN(parsedArtistId) || isNaN(parsedOrganizerProfileId)) {
-      return res.status(400).json({
-        success: false,
-        message: "Input numerik tidak valid.",
-      });
-    }
-
-    const newEvent = await prisma.event.create({
-      data: {
-        name,
-        bannerUrl,
-        city,
-        venue,
-        date: formattedDate,
-        category,
-        price: parsedPrice,
-        description,
-        availableSeats: parsedAvailableSeats,
-        type,
-        artistId: parsedArtistId,
-        organizerProfileId: parsedOrganizerProfileId,
-      },
-    });
-
-    res.status(201).json({
-      success: true,
-      message: "Created successfully.",
-      data: newEvent,
-    });
-  } catch (error) {
-    console.error("Create Event Error:", error); // penting
-
     const { organizerProfileId } = req.body.payload;
 
     const findOrganizerById = await prisma.organizerProfile.findFirst({
@@ -94,6 +40,102 @@ export const getCreatedEvents = async (
       success: true,
       message: "Displayed events successfully",
       data: events,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {
+      name,
+      bannerUrl,
+      city,
+      venue,
+      date,
+      category,
+      price,
+      description,
+      availableSeats,
+      type,
+      artistId,
+      organizerProfileId,
+    } = req.body;
+
+    // Konversi dan validasi data
+    const formattedDate = new Date(date); // cukup langsung new Date
+    const parsedPrice = Number(price);
+    const parsedAvailableSeats = Number(availableSeats);
+    const parsedArtistId = Number(artistId);
+    const parsedOrganizerProfileId = Number(organizerProfileId);
+
+    if (
+      isNaN(parsedPrice) ||
+      isNaN(parsedAvailableSeats) ||
+      isNaN(parsedArtistId) ||
+      isNaN(parsedOrganizerProfileId)
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Input numerik tidak valid.",
+      });
+    }
+
+    const newEvent = await prisma.event.create({
+      data: {
+        name,
+        bannerUrl,
+        city,
+        venue,
+        date: formattedDate,
+        category,
+        price: parsedPrice,
+        description,
+        availableSeats: parsedAvailableSeats,
+        type,
+        artistId: parsedArtistId,
+        organizerProfileId: parsedOrganizerProfileId,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Created successfully.",
+      data: newEvent,
+    });
+  } catch (error) {
+    console.error("Create Event Error:", error); // penting
+    next(error);
+  }
+};
+
+export const eventDisplayLists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const displayEvents = await prisma.event.findMany({
+      select: {
+        id: true,
+        name: true,
+        bannerUrl: true,
+        city: true,
+        venue: true,
+        date: true,
+        category: true,
+        description: true,
+        availableSeats: true,
+      },
+    });
+
+    res.status(200).json({
+      data: displayEvents,
     });
   } catch (error) {
     next(error);
--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -1,16 +1,13 @@
-
 import { Router } from "express";
 import { jwtDecode } from "../middlewares/jwt.decode";
 import { getCreatedEvents } from "../controllers/event.controller";
-import { Router } from "express";
 import { createEvent } from "../controllers/event.controller";
-import { eventDisplayLists } from "../controllers/user.controller";
+import { eventDisplayLists } from "../controllers/event.controller";
 
 const eventRouter = Router();
 
 eventRouter.get("/all-events", eventDisplayLists);
 eventRouter.post("/create-events", createEvent);
 eventRouter.get("/", jwtDecode, getCreatedEvents);
-
 
 export default eventRouter;
